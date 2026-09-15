8772e20445f681d96183eceb1bc780ff99ec222f
github-action-benchmark <github@users.noreply.github.com>
2026-09-15 14:30:27 +0000

    add Orchard Benchmarks (cargo) benchmark result for 3c4a14a2b7eb962bbe9e7f06cdd6fea148e737d6

---
+++ dev/bench/data.js
@@ -0,0 +1,80 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1789482627647,
+  "repoUrl": "https://github.com/auzum197/auzum-orchard",
+  "entries": {
+    "Orchard Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "dorianvp@zingolabs.org",
+            "name": "dorianvp",
+            "username": "dorianvp"
+          },
+          "committer": {
+            "email": "dorianvp@zingolabs.org",
+            "name": "dorianvp",
+            "username": "dorianvp"
+          },
+          "distinct": true,
+          "id": "3c4a14a2b7eb962bbe9e7f06cdd6fea148e737d6",
+          "message": "chore: pin the note encryption fork in the justfile",
+          "timestamp": "2026-09-14T02:01:25-03:00",
+          "tree_id": "8cbaf8f071588c870e7b72d54459e9405c35c8f7",
+          "url": "https://github.com/auzum197/auzum-orchard/commit/3c4a14a2b7eb962bbe9e7f06cdd6fea148e737d6"
+        },
+        "date": 1789482626618,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "ironwood-payment-proving/1",
+            "value": 417015190,
+            "range": "± 4526364",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ironwood-payment-proving/2",
+            "value": 506025099,
+            "range": "± 4349035",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ironwood-payment-proving/3",
+            "value": 704611158,
+            "range": "± 1851670",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ironwood-payment-proving/4",
+            "value": 893468633,
+            "range": "± 7597352",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ironwood-payment-verifying/1",
+            "value": 6859740,
+            "range": "± 84913",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ironwood-payment-verifying/2",
+            "value": 7332243,
+            "range": "± 80127",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ironwood-payment-verifying/3",
+            "value": 7856953,
+            "range": "± 85009",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ironwood-payment-verifying/4",
+            "value": 8353512,
+            "range": "± 121429",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
